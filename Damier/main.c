31001973a6b4625a311d9c0470070fb9ba2cb797
nico97118 <nico97118@gmail.com>
2016-01-06 21:47:51 +0100

    Initial Commit

---
+++ Damier/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Damier
+//
+//  Created by Nicolas Loridon on 06/01/2016.
+//  Copyright © 2016 Nicolas Loridon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
